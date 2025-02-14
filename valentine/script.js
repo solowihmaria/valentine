340a9c5bc13b6c4f9b48d315c65a0a86da77359b
solowihmaria <solowihmaria@gmail.com>
2025-02-14 14:43:19 +0300

    feat: add music

--- valentine/script.js
+++ valentine/script.js
@@ -1,20 +1,17 @@
-let currentPage = 0;
-const pages = document.querySelectorAll(".page");
-const finalPage = document.getElementById("final-page");
-const card = document.getElementById("card");
-const front = document.getElementById("front");
+window.addEventListener("DOMContentLoaded", () => {
+  const audio = document.getElementById("bg-music");
 
-front.addEventListener("click", () => {
-  if (currentPage < pages.length) {
-    pages[currentPage].style.display = "block";
-    if (currentPage > 0) pages[currentPage - 1].style.display = "none";
-    currentPage++;
-  } else {
-    finalPage.style.display = "block";
-    pages[currentPage - 1].style.display = "none";
-  }
-});
+  audio.play().catch(() => {
+    console.log("Автовоспроизведение заблокировано. Запуск по клику.");
+  });
 
-finalPage.addEventListener("click", () => {
-  location.reload();
+  document.addEventListener(
+    "click",
+    () => {
+      if (audio.paused) {
+        audio.play();
+      }
+    },
+    { once: true }
+  );
 });
